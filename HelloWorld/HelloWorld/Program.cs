5e45403fae4bdacaa7ab8b3155a0d5644ca85664
Jordan Smellie <jsmellie@gmail.com>
2014-06-26 11:02:01 -0400

    First Version
    Created program to read out Hello World, take user input, and print it
    to console until dismissed by user.

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string UserInput;
+
+            Console.WriteLine("Hello, world! Say something.");
+            UserInput = Console.ReadLine();
+            Console.WriteLine(UserInput);
+            Console.WriteLine("Press any key to end program.");
+            Console.ReadLine();
+        }
+    }
+}
